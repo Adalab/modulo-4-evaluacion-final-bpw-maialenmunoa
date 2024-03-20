d26a3502fe0ef8cc6b09d589d832f9c074d7c479
maialenmunoa <maialenmunoa@gmail.com>
2024-03-20 23:30:05 +0100

    add users schema

--- data/schema.sql
+++ data/schema.sql
@@ -1,5 +1,4 @@
 /* Tabla recetas */
-
 CREATE TABLE `recetas_db`.`recetas` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(265) NOT NULL,
@@ -7,3 +6,12 @@ CREATE TABLE `recetas_db`.`recetas` (
   `instrucciones` LONGTEXT NOT NULL,
   PRIMARY KEY (`id`));
 
+
+/* Usuarios */
+CREATE TABLE `usuarios_db`.`usuarios` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(265) NOT NULL,
+  `nombre` VARCHAR(265) NOT NULL,
+  `password` VARCHAR(265) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
