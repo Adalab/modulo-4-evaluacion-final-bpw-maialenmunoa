db536a1f500309ea43d10f5ee61fc94b2549de0e
maialenmunoa <maialenmunoa@gmail.com>
2024-03-23 21:54:00 +0100

    fixed fetchs

--- WEB/src/pages/LoginForm.jsx
+++ WEB/src/pages/LoginForm.jsx
@@ -12,7 +12,7 @@ const LoginForm = ({ setToken }) => {
     event.preventDefault();
 
     try {
-      const response = await fetch("/login", {
+      const response = await fetch("//localhost:3000/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
